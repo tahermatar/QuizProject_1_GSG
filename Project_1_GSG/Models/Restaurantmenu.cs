89afd1723085af0f4a6920655c2c5229cb0be79c
Almajd-PC <Almajd-PC@DESKTOP-NS1NQEO>
2022-09-17 00:48:18 +0300

    add convert price

--- Project_1_GSG/Models/Restaurantmenu.cs
+++ Project_1_GSG/Models/Restaurantmenu.cs
@@ -15,7 +15,7 @@ namespace Project_1_GSG.Models
         public int Id { get; set; }
         public string MealName { get; set; }
         public float PriceInNis { get; set; }
-        public float PriceInUsd { get; set; }
+        public float PriceInUsd => (float)(PriceInNis / 3.5);
         public int Quantity { get; set; }
         public int RestaurantId { get; set; }
         public DateTime CreatedDateUTC { get; set; }
